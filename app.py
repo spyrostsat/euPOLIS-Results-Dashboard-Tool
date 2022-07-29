1a47e122f88fc6855e6b4e5db08f648d9c883417
Spyros Tsattalios <spyros.tsattalios@gmail.com>
2022-07-29 21:19:50 +0300

    new commit

--- app.py
+++ app.py
@@ -24,8 +24,21 @@ app.config['SECRET_KEY'] = '61eeaaa0602a3b02cf95cedfed635601'
 @app.route("/")
 @app.route("/home")
 def home():
-    # print(baseline_scenario_data["general_info"]["pilot site name"])
-    return render_template('home.html', title="Home")
+    # in the Home Page i will just use the basic site's information received from the baseline_scenario json file
+
+    site_name = baseline_scenario_data["general_info"]["pilot site name"]
+    site_location = baseline_scenario_data["general_info"]["pilot site location"]
+    simulation_period = baseline_scenario_data["general_info"]["Simulation period"]
+    site_surface = baseline_scenario_data["general_params"]["total_site_surface"]["param_val"]
+    existing_green_surfaces = baseline_scenario_data["general_params"]["existing_green_surfaces"]["param_val"]
+
+
+    # in html's jinja syntax i don't need json formatting, but i need it in js's jinja syntax
+    baseline_scenario_data_js = json.dumps(baseline_scenario_data)
+    return render_template('home.html', title="Home", baseline_scenario_data=baseline_scenario_data,
+                           baseline_scenario_data_js=baseline_scenario_data_js, site_name=site_name, site_location=site_location,
+                           simulation_period=simulation_period, site_surface=site_surface, existing_green_surfaces=existing_green_surfaces)
+
 
 @app.route("/baseline-scenario")
 def baseline_scenario():
