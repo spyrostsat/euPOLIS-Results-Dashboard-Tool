5f3b5e3364e930ef986eb461779fa9cea4dcfe1f
Spyros Tsattalios <spyros.tsattalios@gmail.com>
2022-08-01 20:30:08 +0300

    new commit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, flash, redirect, request
 import json
+from datetime import datetime
 
 with open("json_files/baseline_json.json", "r") as f:
     baseline_scenario_data = json.load(f)
@@ -37,6 +38,26 @@ def home():
 
 @app.route("/baseline-scenario")
 def baseline_scenario():
+    dates_ts = baseline_scenario_data["Output_TS"]["date"]
+    dates_years_ts = [datetime.strptime(date, '%Y-%m-%d').year for date in dates_ts]
+    dates_months_ts = [datetime.strptime(date, '%Y-%m-%d').month for date in dates_ts]
+
+
+    total_gw_ts = baseline_scenario_data["Output_TS"]["total_gw_logger_id_108"]
+    imprv_roofs_runoff_ts = baseline_scenario_data["Output_TS"]["imprv_roofs_runoff_logger_id_215"]
+    total_runoff_ts = baseline_scenario_data["Output_TS"]["total_runoff_logger_id_129"]
+    total_demand_ts = baseline_scenario_data["Output_TS"]["total_demand_logger_id_54"]
+    total_wastewater_ts = baseline_scenario_data["Output_TS"]["total_wastewater_logger_id_55"]
+    rainfall_ts = baseline_scenario_data["Input_TS"]["Rainfall_series_id_1633_group_id_15"]
+    impervious_surface = baseline_scenario_data["model_params"]["imprv_roofs_id_8"]["param_val"]
+    total_site_surface = baseline_scenario_data["general_params"]["total_site_surface"]["param_val"]
+    existing_green_surfaces = baseline_scenario_data["general_params"]["existing_green_surfaces"]["param_val"]
+
+    total_demand_baseline_ts = baseline_scenario_data["Output_TS_baseline"]["total_demand_logger_id_54"]
+    total_runoff_baseline_ts = baseline_scenario_data["Output_TS_baseline"]["total_runoff_logger_id_129"]
+    total_wastewater_baseline_ts = baseline_scenario_data["Output_TS_baseline"]["total_wastewater_logger_id_55"]
+
+
     return render_template('baseline_scenario.html', title="Baseline Scenario")
 
 
